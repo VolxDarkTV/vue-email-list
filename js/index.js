ceeb51a083e7d5bdd3642fbb28bd76ea2e54dd53
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-06 15:40:01 +0100

    Upload files and print emails

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const { createApp } = Vue;
+
+createApp({
+    
+    data (){
+        return{
+            email: [],
+        }
+    },
+    methods:{
+        generateEmailLi(){
+            
+        }
+    },
+    mounted(){
+        for (let index = 1; index <= 10; index++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data.response);
+                this.email.push(response.data.response);
+            });
+        }
+    }
+
+}).mount('#app')
